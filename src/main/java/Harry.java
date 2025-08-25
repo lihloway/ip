1be106841368315c95ba941774045a0a4111fa4f
lihloway <80195957+lihloway@users.noreply.github.com>
2025-08-25 17:05:41 +0800

    feat: Echo

--- src/main/java/Harry.java
+++ src/main/java/Harry.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
+
 public class Harry {
     public static void main(String[] args) {
         Harry chatbot = new Harry();
-        chatbot.greet();
     }
 
     Harry() {
+        Scanner scanner = new Scanner(System.in);
+        say_hello();
+        boolean exit = false;
+        String name = "";
+        while(!exit){
+            name = scanner.nextLine();
+            if (name.equals("bye")){
+                exit = true;
+            }
+            else{
+                print_line();
+                System.out.println("Huh? "+name+"?!");
+                print_line();
+            }
+        }
+        say_goodbye();
     }
 
-    private void greet() {
+    private void say_hello() {
         print_line();
         System.out.println("Hello from\n" + get_name());
         System.out.println("Is there anything I can do for you?");
         print_line();
-
+    }
+    private void say_goodbye() {
         print_line();
         System.out.println("Nice talking to you! I hope to see you again.");
         print_line();
